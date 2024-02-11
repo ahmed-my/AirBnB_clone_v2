96d655e251c515a3a91c6b33dcb598e1e93a106f
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-11 01:05:12 +0000

    Deploy Static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 # script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
@@ -11,41 +10,18 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-sudo chown -R ubuntu:ubuntu /data/
-sudo tee /data/web_static/current/index.html <<EOF
-<html>
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-EOF
-sudo rm -rf /data/web_static/current
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-echo "server {
-   listen 80 default_server;
-   listen [::]:80 default_server;
-   server_name _;
-   add_header X-Served-By \"\$hostname\";
-   root /var/www/html;
-   index index.html;
+sudo chown -R ubuntu:ubuntu /data/
 
-   location /redirect_me {
-      return 301 https://youtube.com/;
-   }
-   location /hbnb_static {
-      alias /data/web_static/current/;
-      index index.html index.htm;
-   }
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-   error_page 404 /404.html;
-   location /404 {
-      root /var/www/html;
-      internal;
-   }
-}" | sudo tee /etc/nginx/sites-enabled/default
-
-sudo nginx -t
 sudo service nginx restart
