dd3b2f7a72fb7f83b2446aefba678ff1be05cf63
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-14 20:03:54 +0000

    Deploy Static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
 Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers.
+distributes an archive to the web servers
+
 execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 """
 
-from fabric.api import env, local, put, run, puts
+from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-
-env.hosts = ['54.164.133.175', '18.210.18.243']
+env.hosts = ['52.201.229.18', '52.23.178.155']
 
 
 def do_pack():
@@ -21,7 +21,7 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except Exception as e:
+    except:
         return None
 
 
@@ -42,7 +42,7 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception as e:
+    except:
         return False
 
 
@@ -51,7 +51,4 @@ def deploy():
     archive_path = do_pack()
     if archive_path is None:
         return False
-    result = do_deploy(archive_path)
-    if result:
-        print("New version deployed!")
-    return result
+    return do_deploy(archive_path)
