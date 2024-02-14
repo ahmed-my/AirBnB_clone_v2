8d79a098bfea72e899a66fbe88b84825a47e2683
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-14 19:15:09 +0000

    Deploy Static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -49,7 +49,7 @@ def do_deploy(archive_path):
 def deploy():
     """creates and distributes an archive to the web servers"""
     archive_path = do_pack()
-    if not archive_path:
+    if archive_path is None:
         return False
     result = do_deploy(archive_path)
     if result:
