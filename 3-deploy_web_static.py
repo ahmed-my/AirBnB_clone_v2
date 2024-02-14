cc007628f6c22ee1ca3cc81ecb60fb325c14b380
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-14 20:17:02 +0000

    Deploy Static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
+distributes an archive to the web servers.
 
 execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 """
@@ -10,6 +10,7 @@ from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
 env.hosts = ['54.164.133.175', '18.210.18.243']
+env.user = 'ubuntu'
 
 
 def do_pack():
@@ -41,6 +42,7 @@ def do_deploy(archive_path):
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        print("New version deployed!")
         return True
     except Exception as e:
         return False
