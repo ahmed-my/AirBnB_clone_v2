3e09a3037a25c590d2985520270cf4fcdae01b3c
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-17 22:48:19 +0000

    web framework

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,22 +5,22 @@ from models import storage
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+my_web_app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
+@my_web_app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
+    """prints an HTML page with a list of all states and related cities.
     """
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
 
 
-@app.teardown_appcontext
+@my_web_app.teardown_appcontext
 def teardown(exc):
     """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    my_web_app.run(host="0.0.0.0", port=5000)
