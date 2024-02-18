fd728c41acfbd36f15cd73bed1f4ebc701e61c33
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-18 14:46:08 +0000

    web framework

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,10 +5,10 @@ from models import storage
 from flask import Flask
 from flask import render_template
 
-my_web_app = Flask(__name__)
+app = Flask(__name__)
 
 
-@my_web_app.route("/states_list", strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
     """prints an HTML page with a list of all State objects in DBStorage.
     """
@@ -16,11 +16,11 @@ def states_list():
     return render_template("7-states_list.html", states=states)
 
 
-@my_web_app.teardown_appcontext
-def teardown(exception):
+@app.teardown_appcontext
+def teardown(exc):
     """closes the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    my_web_app.run(host="0.0.0.0", port='5000')
+    app.run(host="0.0.0.0", port='5000')
